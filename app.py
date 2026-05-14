7cb7c9038dddf46fff78c23398d1538784d3c5d9
hayssamchebli-chh <hayssam.chebli@chhholding.com>
2026-05-14 15:41:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -221,6 +221,7 @@ with st.sidebar:
     st.markdown(
         f"""
         By default, the app applies this factor to all items: **Default FACTOR = {default_factor}**
+        
         You may optionally upload a Safety Factor File to use different factors per item.
         """
     )
